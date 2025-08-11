5d72f5697e7b49d0df574a0a12bb84f68e07d9c2
Karina Sanchez <kaeusaju@hotmail.com>
2025-08-11 12:55:33 +0200

    print alphabet completo

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -6,7 +6,26 @@
 /*   By: kakahuate <kakahuate@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 12:12:22 by kakahuate         #+#    #+#             */
-/*   Updated: 2025/08/11 12:43:10 by kakahuate        ###   ########.fr       */
+/*   Updated: 2025/08/11 12:54:07 by kakahuate        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		write(1, &letter, 1);
+		letter++;
+	}
+}
+
+/* int main(void)
+{
+	ft_print_alphabet();
+	return (0);
+} */
